ad05bbac9a2b2721a412bd1d98e71cf099eeac26
Sein <47228978+goatmew@users.noreply.github.com>
2019-02-03 14:22:14 -0300

    Added icons info

--- Board Explorer/About.designer.cs
+++ Board Explorer/About.designer.cs
@@ -30,6 +30,7 @@
         {
             this.btnDonate = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnDonate
@@ -51,11 +52,21 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "Simple e621 Browser";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 229);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(222, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Icons by: https://www.fatcow.com/free-icons";
+            // 
             // About
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(263, 251);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnDonate);
             this.MaximizeBox = false;
@@ -73,5 +84,6 @@
 
         private System.Windows.Forms.Button btnDonate;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
